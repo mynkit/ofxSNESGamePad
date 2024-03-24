ba287caec6f9fe30f6ac5c0c4bf398edb1e69844
mynkit <dcm5124289@gmail.com>
2024-03-24 21:50:31 +0900

    first commit

---
+++ addons.make
@@ -0,0 +1,2 @@
+ofxGLFWJoystick
+ofxJoystick
---
+++ src/ofApp.cpp
@@ -0,0 +1,66 @@
+
+#include "ofApp.h"
+
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+  joy_.setup(GLFW_JOYSTICK_1);
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+  // chack all button for push
+  for (int i = 0; i < joy_.getButtonNum(); i++) {
+    if (joy_.isPressed(i)) {
+      ofLog() << "pressed :" << i;
+    }
+    if (joy_.isPushing(i)) {
+      ofLog() << "pushing :" << i;
+    }
+    if (joy_.isRelease(i)) {
+      ofLog() << "release :" << i;
+    }
+  }
+  
+  // get left X axis
+  ofLog() << "left X axis :" << joy_.getAxis(0);
+  
+  // get left Y axis
+  ofLog() << "left Y axis :" << joy_.getAxis(1);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {}
